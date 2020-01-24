f50d352e1a60101ffce1cc601604ac4d505d7b56
RajaShekar <yadavallirajashekar@gmail.com>
2020-01-24 15:50:53 +0530

    Description for functions

--- modules/changeAnn.py
+++ modules/changeAnn.py
@@ -1,12 +1,18 @@
+# Author:Rajashekar Yadavalli
+# URL: https://github.com/RajashekarY
+# Manipulate annotations using the following functions
 import os
 import sys
 import argparse
 import shutil
 
-def changeClass(FolderPath,existingClass,toClass): 
-    txtFiles = [txt for txt in os.listdir(FolderPath) if txt.endswith('.txt')] 
+def changeClass(folderPath,existingClass,toClass): #folderPath:where annotations are 
+    '''if you have generated annotations using Openimages tool kit for custom 
+    objects and you want to use the same data but annotatiosn for yolo need a 
+    change in class num then this script will come handy'''
+    txtFiles = [txt for txt in os.listdir(folderPath) if txt.endswith('.txt')] 
     for afile in txtFiles:
-        afile=os.path.join(FolderPath,afile) 
+        afile=os.path.join(folderPath,afile) 
         fileOpen = open(afile,'r') 
         lines=[line.strip() for line in fileOpen.readlines()] 
         fileOpen.close()
@@ -19,6 +25,9 @@ def changeClass(FolderPath,existingClass,toClass):
         writeFile.close()                                                                                                      
 
 def copyAnn(fromFolder,toFolder):
+        '''if you have to copy files downloaded using Openimages tool kit for custom 
+    objects and you want to copy annotations into single folder without overwriting 
+    them then this script comes handy'''
     fromFolder = os.path.join(fromFolder,'Label')
     fromTXT = [txt for txt in os.listdir(fromFolder) if txt.endswith('.txt')]
     toTXT = [txt for txt in os.listdir(toFolder) if txt.endswith('.txt')]
